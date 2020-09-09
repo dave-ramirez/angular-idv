d84ea1466efa792a797c289c3ef15a82e4ac1863
Oscar David Paredes Ramirez <OSCARP@interbanco.com.py>
2020-09-09 13:45:43 -0400

    Se modifico y preparo el proyecto para pasar a la rama master

--- src/app/shared/communicator/communicator.service.ts
+++ src/app/shared/communicator/communicator.service.ts
@@ -1,40 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ICommunicatorService } from './communicator';
-import { Observable } from 'rxjs';
-import { VoxelRouterCommunicatorService } from './voxel/voxel-router.communicator.service';
-import { VoxelNativeCommunicatorService } from './voxel/voxel-native.communicator.service';
+import { NativeCommunicatorService } from './native/voxel-native.communicator.service';
 
 @Injectable()
 export class CommunicatorService implements ICommunicatorService {
 
-  name: string;
-
-  strategy: ICommunicatorService;
-
   constructor(
-    private voxelRouter: VoxelRouterCommunicatorService,
-    private voxelNative: VoxelNativeCommunicatorService
-  ) {
-    this.strategy = (window as any).native ? voxelNative : this.voxelRouter; // 
-    console.log('Using strategy: ', this.strategy.name);
-  }
-
-  routerSelect() {
-    this.strategy = this.voxelRouter;
-    console.log('Using strategy: ', this.strategy.name);
-  }
-
-  nativeSelect() {
-    this.strategy = this.voxelNative;
-    console.log('Using strategy: ', this.strategy.name);
-  }
-
-  doRequestRouter(operation: string, body: any = {}): Observable<any> {
-    return this.strategy.doRequestRouter(operation, body);
-  }
+    private native: NativeCommunicatorService
+  ) { }
 
   doRequestNative(operation: any, HTTP?: any) {
-    return this.strategy.doRequestNative(operation, HTTP);
+    return this.native.doRequestNative(operation, HTTP);
   }
 
 }
